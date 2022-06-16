a4888b0fcf4cb0331eb24c2c75ce306825f5879f
Sepehr Jalayer <jalayersepehr@gmail.com>
2022-06-15 23:41:20 -0400

    fixed the python script
    Signed-off-by: Sepehr Jalayer <jalayersepehr@gmail.com>

--- Assets/Scripts/PythonScripts/main.py
+++ Assets/Scripts/PythonScripts/main.py
@@ -1,5 +1,6 @@
 # This is a sample Python script.
 
+import bct
 import numpy as np
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
@@ -11,11 +12,11 @@ df_region_color = pd.read_csv('HCP-MMP1_RegionColor.csv', skipfooter=19, engine=
 df_region_list = pd.read_csv('HCP-MMP1_UniqueRegionList.csv')
 df_main = pd.read_csv('hcpmmp1.csv', skipfooter=19, header=None, engine='python')
 ndarray_sub = np.asarray(list(df_main.get(0)))
-ndarray_main = np.empty((1, 360))
-for x in range(ndarray_sub.size):
+ndarray_main = np.resize(np.asarray(dtype=float, a=ndarray_sub[0].split(' ')), (1, 360))
+for x in range(1, ndarray_sub.size):
     tmp: ndarray = np.resize(np.asarray(dtype=float, a=ndarray_sub[x].split(' ')), (1, 360))
     # print(tmp)
-    np.concatenate((ndarray_main, tmp))
+    ndarray_main = np.append(ndarray_main, tmp, axis=0)
 
 # ndarray_main = np.reshape(ndarray_main,(359,359))
 # columns = range(0,360)
@@ -33,17 +34,20 @@ print(ndarray_main)
 
 # print(clean_df)
 
-print('Result of the algorithms:')
+print('Result of the algorithms:\n=========================')
 # bct.assortativity_wei(CIJ=ndarray_main,flag=0)
-# print('Result of the algorithms:'+ bct.assortativity_wei(CIJ=df_main))
-
+print('Result of the assortativity_wei:' + bct.assortativity_wei(CIJ=ndarray_main, flag=0).__str__())
+print('Result of the assortativity_wei:' + bct.assortativity_wei(CIJ=ndarray_main, flag=0).__str__())
+print('Result of the assortativity_wei:' + bct.assortativity_wei(CIJ=ndarray_main, flag=0).__str__())
+print('Result of the assortativity_wei:' + bct.assortativity_wei(CIJ=ndarray_main, flag=0).__str__())
+print('Result of the assortativity_wei:' + bct.assortativity_wei(CIJ=ndarray_main, flag=0).__str__())
 
 ### addind the indices for better understanding
 # result_index = df_region_list.columns
 
 # print(result_index)
 
-#result = pd.DataFrame(data=result, index=result_index).T
-#result.to_csv('res.csv')
+# result = pd.DataFrame(data=result, index=result_index).T
+# result.to_csv('res.csv')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
